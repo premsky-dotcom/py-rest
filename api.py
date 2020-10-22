989a4092ea441f14f629f7b0e16763470975b126
premsky-dotcom <73269526+premsky-dotcom@users.noreply.github.com>
2020-10-22 09:51:02 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+app.config.from_object('configuration.DevelopmentConfig')
+
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'quantity')
+
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+
+@app.route("/product", methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    new_product = Product(name, description, price, quantity)
+    db.session.add(new_product)
+    db.session.commit()
+    return product_schema.jsonify(new_product)
+
+
+@app.route("/product", methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+
+@app.route("/product/<id>", methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+
+@app.route("/product/<id>",methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+
+@app.route("/product/<id>", methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+
+@app.route("/", methods=["GET"])
+def get():
+    return jsonify("{'msg':'Hello World'}")
+
+
+if __name__ == "__main__":
+    app.run()
